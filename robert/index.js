fa0b45d788d8fe8cef0a957664dd75a9a90b6228
Till Prochaska <mail@tillprochaska.de>
2019-05-04 15:26:04 +0200

    Clean up

--- robert/index.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', async () => {
-  console.log('Hello!')
-  const response = await fetch('https://kas-workshop-api.netlify.com/polls/0.json')
-  const json = await response.json()
-  console.log(json)
-})
