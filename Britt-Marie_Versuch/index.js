0d9bf32490bc57aed899b80e369d1cfccfcc01d6
Britt-Marie Lakämper <mail@bmlakaemper.de>
2019-05-05 17:06:36 +0200

    Update index.js

--- Britt-Marie_Versuch/index.js
+++ Britt-Marie_Versuch/index.js
@@ -1,2 +1,17 @@
-console.log('Hello World!')
-data = {"title": "Änderung des Tierschutzgesetzes"}
+console.log('Hello World!');
+data = {"title": "Änderung des Tierschutzgesetzes"};
+var el = document.querySelector("input");
+console.log(el);
+el.addEventListener('input', function (evt) {
+  fetch('https://kas-workshop-api.netlify.com/polls/0.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(PollData) {
+      var votes = PollData.votes
+      console.log(votes[0].name);
+      const result = votes.filter( item => item.vote === "ja");
+
+console.log(result);
+    });
+});
