d0ccdba6028948dcbea45e3b476d9a169558bb8a
janagl <jana.glose@gmx.de>
2019-05-05 17:36:45 +0200

    index.html

--- Jana/index.js
+++ Jana/index.js
@@ -7,5 +7,22 @@ fetch('https://kas-workshop-api.netlify.com/polls/2.json')
   .then(function(myJson) {
     console.log(myJson.title);
     console.log(myJson.votes[0].name);
+   
+myJson.votes.forEach(function(vote){
+	console.log(vote);
+	 document.getElementById("name").innerHTML += vote.name+"<br>" 
+}) 
+    document.getElementById("ueberschrift").innerHTML = myJson.title
+
+
+
   })
+
+obj = {table: "votes", limit: 704 };
+
+
+
+
+
+
 
