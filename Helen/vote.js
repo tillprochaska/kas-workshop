b350ed206a050f8ad97f104edc7d67f98498ac98
HelenBielawa <helen.bielawa@web.de>
2019-05-05 16:07:00 +0200

    Current code base

--- Helen/vote.js
+++ Helen/vote.js
@@ -1,1 +1,12 @@
 console.log('Hello World!')
+
+fetch('https://kas-workshop-api.netlify.com/polls/2.json')
+.then(function(response) {
+	return response.json();
+})
+.then(function(myJson) {
+	console.log(JSON.stringify(myJson));
+
+	console.log(myJson["title"]);
+	console.log(myJson.title);
+});
